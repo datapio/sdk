f4f3cf8194a91186d87c922bb86e0b64bc9debc6
David Delassus <david.jose.delassus@gmail.com>
2021-05-22 20:05:34 +0200

    :wrench: add netlify/hugo configuration #www

---
+++ assets/build-doc.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -eux
+
+(cd sources/node/amqp-engine && yarn install && yarn doc)
+(cd sources/node/k8s-operator && yarn install && yarn doc)
+
+mkdir -p www/static/docs
+cp -Rv sources/node/amqp-engine/docs www/static/docs/amqp-engine
+cp -Rv sources/node/k8s-operator/docs www/static/docs/k8s-operator
+
+hugo --minify -s www
