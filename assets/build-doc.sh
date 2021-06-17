f145628688e60bff399ee89b59697a4b34938c1c
David Delassus <david.jose.delassus@gmail.com>
2021-06-17 12:40:15 +0200

    :construction_worker: :bug: install yarn when building documentation

--- assets/build-doc.sh
+++ assets/build-doc.sh
@@ -2,8 +2,10 @@
 
 set -eux
 
-(cd sources/node/amqp-engine && npm install && npm run doc)
-(cd sources/node/k8s-operator && npm install && npm run doc)
+npm install -g yarn
+
+(cd sources/node/amqp-engine && yarn install && yarn run doc)
+(cd sources/node/k8s-operator && yarn install && yarn run doc)
 
 mkdir -p www/static/docs
 cp -Rv sources/node/amqp-engine/docs www/static/docs/amqp-engine
